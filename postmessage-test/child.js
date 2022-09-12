e03b67cb6958d4ae1ab808c1caefe2df54e4dfaa
Paul Titcomb <p.titcomb@savethechildren.org.uk>
2022-09-12 17:13:52 +0100

    Extra logging for testing

--- postmessage-test/child.js
+++ postmessage-test/child.js
@@ -3,7 +3,9 @@ window.addEventListener('message', event => {
 	var msgFromParent = event.data;
 	if (msgFromParent.includes('cjdOTPermissions-')) {
 		otPermissions = msgFromParent.replace('cjdOTPermissions-', '');
-		console.log('yo yo yo we got a message');
+		console.log('==============');
+		console.log(`we've got a message from the parent page`);
 		console.log(otPermissions);
+		console.log('==============');
 	}
 });
