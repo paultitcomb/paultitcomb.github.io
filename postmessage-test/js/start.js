12ad8293f256e4428a5f93c93410d3b6f884065e
Paul Titcomb <p.titcomb@savethechildren.org.uk>
2022-09-27 11:32:04 +0100

    Now with postmessage listening

--- postmessage-test/js/start.js
+++ postmessage-test/js/start.js
@@ -3,3 +3,8 @@ const openBtn = document.querySelector('.open-page-btn');
 openBtn.addEventListener('click', e => {
 	const openedWin = window.open('new_page.html', 'jgPayinForm');
 });
+
+window.addEventListener('message', e => {
+	console.log('message received');
+	console.log(e.data);
+});
