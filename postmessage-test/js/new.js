5b6ad3e18cbb3cbeb8f8417bf211c23f2237451b
Paul Titcomb <p.titcomb@savethechildren.org.uk>
2022-09-29 11:32:39 +0100

    Comment out onload event

--- postmessage-test/js/new.js
+++ postmessage-test/js/new.js
@@ -1,10 +1,10 @@
 let opener = null;
 const origWinBtn = document.querySelector('.original-window-btn');
 
-window.addEventListener('load', e => {
-	opener = window.opener;
-	opener.postMessage('popup opened', '*');
-});
+// window.addEventListener('load', e => {
+// 	opener = window.opener;
+// 	opener.postMessage('popup opened', '*');
+// });
 
 origWinBtn.addEventListener('click', e => {
 	window.postMessage('hello from the opened window', '*');
