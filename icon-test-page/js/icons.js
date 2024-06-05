1332bbf7a54ba4a69fc3b55cc1ce32318db5c93b
Paul Titcomb <p.titcomb@savethechildren.org.uk>
2024-06-05 17:06:20 +0100

    Updated selector

--- icon-test-page/js/icons.js
+++ icon-test-page/js/icons.js
@@ -1,6 +1,6 @@
 const icons = [
 	{
-		selector: 'search-wrapper',
+		selector: '.search-wrapper',
 		svgCode: `<svg class="icon-search" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="14" cy="14" r="8" stroke="#AE2015" stroke-width="4" /><path d="M24.5858 27.4142c.781.7811 2.0474.7811 2.8284 0 .7811-.781.7811-2.0474 0-2.8284l-2.8284 2.8284Zm-6-6 6 6 2.8284-2.8284-6-6-2.8284 2.8284Z" fill="#AE2015"/></svg>`,
 		colors: ['standard', 'white', 'dark-red', 'plum', 'black']
 	}
