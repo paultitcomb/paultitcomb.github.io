7a97aa5a6b9a2aa2d1fdf112be8514cf713745cb
Paul Titcomb <p.titcomb@savethechildren.org.uk>
2024-06-06 17:29:51 +0100

    Added class attributes

--- icon-test-page/js/icons.js
+++ icon-test-page/js/icons.js
@@ -16,17 +16,17 @@ const icons = [
 	},
 	{
 		selector: '.anchor-wrapper',
-		svgCode: `<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.94019 7.0606L6.11177 9.88903M6.11177 9.88903L3.28334 7.0606M6.11177 9.88903V2.11085" stroke="#DA291C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>`,
+		svgCode: `<svg class="icon-anchor-link" width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.94019 7.0606L6.11177 9.88903M6.11177 9.88903L3.28334 7.0606M6.11177 9.88903V2.11085" stroke="#DA291C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>`,
 		colors: ['red', 'white', 'dark-red', 'plum', 'black']
 	},
 	{
 		selector: '.external-link-wrapper',
-		svgCode: `<svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 3H3C2.44772 3 2 3.44738 2 3.99966C2 6.5876 2 10.2574 2 12.003C2 12.5553 2.44772 13 3 13H11C11.5523 13 12 12.5523 12 12V8" stroke="#DA291C" stroke-linejoin="round"/><path d="M9 2H13M13 2V6M13 2L7.5 7.5" stroke="#DA291C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>`,
+		svgCode: `<svg class="icon-external-link" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 3H3C2.44772 3 2 3.44738 2 3.99966C2 6.5876 2 10.2574 2 12.003C2 12.5553 2.44772 13 3 13H11C11.5523 13 12 12.5523 12 12V8" stroke="#DA291C" stroke-linejoin="round"/><path d="M9 2H13M13 2V6M13 2L7.5 7.5" stroke="#DA291C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>`,
 		colors: ['red', 'white', 'dark-red', 'plum', 'black']
 	},
 	{
 		selector: '.alert-wrapper',
-		svgCode: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="6.5" stroke="#DA291C"/><rect x="7" y="4" width="2" height="5" rx="1" fill="#DA291C"/><circle cx="8" cy="11" r="1" fill="#DA291C"/></svg>`,
+		svgCode: `<svg class="icon-alert" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="6.5" stroke="#DA291C"/><rect x="7" y="4" width="2" height="5" rx="1" fill="#DA291C"/><circle cx="8" cy="11" r="1" fill="#DA291C"/></svg>`,
 		colors: ['red', 'white', 'dark-red', 'plum', 'black']
 	}
 ];
