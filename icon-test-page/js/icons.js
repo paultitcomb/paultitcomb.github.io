f2800e6085e9cefc5001522c8e58033a04a79e76
Paul Titcomb <p.titcomb@savethechildren.org.uk>
2024-06-14 16:17:15 +0100

    Insert iframe with styles into page

--- icon-test-page/js/icons.js
+++ icon-test-page/js/icons.js
@@ -1,10 +1,23 @@
-function init() {
+function insertStyles() {
+	const styleTag = document.createElement('style');
+	styleTag.textContent = `.icon-test-page-wrapper{min-height:400vh;position:relative}.icon-test-page-wrapper iframe{border:0;width:80%;height:100%;position:absolute;left:10%}`;
+	document.head.appendChild(styleTag);
+}
+
+function injectIframe() {
 	const iframeWrapper = document.querySelector('.icon-test-page-wrapper');
 
 	const iframe = document.createElement('iframe');
 	iframe.src = 'https://paultitcomb.github.io/icon-test-page';
 
+	// clear out the text
+	iframeWrapper.innerHTML = '';
 	iframeWrapper.insertAdjacentElement('beforeend', iframe);
+}
+
+function init() {
+	insertStyles();
+	injectIframe();
 }
 
 // event listeners
