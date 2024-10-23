27dad3d49d2712cdf0dc94d2be3ef43c6248ec11
Paul Titcomb <4091893+paultitcomb@users.noreply.github.com>
2024-10-23 17:28:46 +0100

    Trying to prevent swipe back

--- history-test/js/main.js
+++ history-test/js/main.js
@@ -1,5 +1,11 @@
 const bodyClass = document.body.className;
 
 if (bodyClass === '3ds-page' || bodyClass === 'ty-page') {
-	history.replaceState(null, '', '/history-test/home.html');
+	// history.replaceState(null, '', '/history-test/home.html');
+	window.addEventListener('beforeunload', function (e) {
+		// Cancel the event and show confirmation dialog
+		e.preventDefault();
+		// Chrome requires returnValue to be set
+		e.returnValue = '';
+	});
 }
